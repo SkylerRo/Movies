04bba2e63885ddb12d9f8db1ab44bb658be63324
SkylerRo <Skylerro20@gmail.com>
2024-10-16 17:42:22 -0400

    Actors done

--- seed/actors.js
+++ seed/actors.js
@@ -2,3 +2,35 @@ const db = require('../db')
 const { Actor, Movie, Reviews } = require('../models')
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+const main = async () => {
+    const actors = [
+        {
+            name: 'Acor Pickle',
+            age: 31,
+            alive: true
+        },
+        {
+            name: 'Ashly Morris',
+            age: 12,
+            alive: false
+        },
+        {
+            name: 'James Hangry',
+            age: 57,
+            alive: true
+        },
+        {
+            name: 'Rabbit Easter',
+            age: 101,
+            alive: false
+        }
+    ]
+    await Actor.insertMany(actors)
+ console.log("Famous Actors!")
+}
+
+const run = async () => {
+    await main()
+    db.close()
+}
